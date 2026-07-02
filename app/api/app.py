bfe1be2b51673457f65ca6062f50fcd30222fb1e
hiba <hibaakhdim51@gmail.com>
2026-07-02 05:24:34 +0100

    Fix Railway port

--- app/api/app.py
+++ app/api/app.py
@@ -423,7 +423,11 @@ def favorite():
 
 
 
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 8080))
 
-if __name__ == "__main__":
- 
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True
+    )
