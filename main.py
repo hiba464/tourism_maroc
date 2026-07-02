b063b1f39420b4e9e6ee3e137640a8b27c4096c5
hiba464 <hibaakhdim51@gmail.com>
2026-07-02 01:31:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from app.main_window import MainWindow
+
+if __name__ == "__main__":
+    print("STEP 1 - START")
+
+    root = tk.Tk()
+
+    print("STEP 2 - ROOT CREATED")
+
+    app = MainWindow(root)
+
+    print("STEP 3 - MAINWINDOW CREATED")
+
+    root.mainloop()
